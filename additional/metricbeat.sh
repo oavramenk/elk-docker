9f49beb996a11d3a37f34bb0a5c44adee977bb39
Oleh Avramenko <y803930@syniverse.com>
2022-06-01 16:50:47 +0300

    some commit

--- additional/metricbeat.sh
+++ additional/metricbeat.sh
@@ -3,7 +3,7 @@
 readonly basedir=/opt/beats
 
 curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.2.0-linux-x86_64.tar.gz
-mkdir -p ${basedir} && tar xzvf metricbeat-8.2.0-linux-x86_64.tar.gz -C ${basedir}/metricbeat
+mkdir -p ${basedir}/metricbeat && tar xzvf metricbeat-8.2.0-linux-x86_64.tar.gz -C ${basedir}/metricbeat
 
 # Copy configuration file for metricbeat
 cp additional/config/metricbeat.yml ${basedir}/metricbeat
