c26b04fee06548f4568d085753d959fd8fe8963f
Oleh Avramenko <y803930@syniverse.com>
2022-06-01 16:17:14 +0300

    some commit

---
+++ addtional/metric_beats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+readonly basedir=/opt/beats
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.2.0-linux-x86_64.tar.gz -o ${basedir}
+tar xzvf ${basedir}/metricbeat-8.2.0-linux-x86_64.tar.gz -C ${basedir}/metricbeat
+
+# Copy configuration file for metricbeat
+cp additional/config/metricbeat.yml ${basedir}/metricbeat
+
+# setup Kibana dashboards and start metricbeat
+metricbeat setup -e
+${basedir}/metricbeat/metricbeat -e
